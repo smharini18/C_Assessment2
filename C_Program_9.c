9e1e912b4e5c4a61b838f165477aeab09a63bd17
smharini18 <smharini18@gmail.com>
2026-06-03 11:10:28 +0530

    Create C_Program_9.c

---
+++ C_Program_9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x,y,z,a;
+    printf("enter a 2 digit number:");
+    scanf("%d",&x);
+    y=x%10;
+    z=x/10;
+    a=(y<z);
+    printf("%d",a);
+    return 0;
+}
